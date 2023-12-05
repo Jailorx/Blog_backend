35a59ca5b095fb51d0fa7dfe88125d521533d2ba
Shubham Bisht <bisht.shubham.emp@gmail.com>
2023-12-05 22:17:36 +0530

    setting up the backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const PORT = 8000;
+require("dotenv").config();
+require("./db");
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.json({ msg: "Everything is working" }));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
